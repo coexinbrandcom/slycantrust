55bd254fdc7b08144227b6c52c95e4e0088a84c4
Coexin Brandcom <61900458+coexinbrandcom@users.noreply.github.com>
2023-06-06 15:19:04 +0530

    Update webflow-elements.js

--- webflow-elements.js
+++ webflow-elements.js
@@ -9,7 +9,7 @@
  */
 
 
-import { WfuLightbox } from 'https://cdn.jsdelivr.net/gh/coexinbrandcom/slycantrust/webflow-element-lightbox.js';
+import { WfuLightbox } from ../coexinbrandcom/slycantrust/webflow-element-lightbox.js';
 
 $(function () {
  
